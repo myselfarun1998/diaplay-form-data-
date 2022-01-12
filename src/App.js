49aa9786c2ae73bad5861a498a14df1c4a99c58a
myselfarun1998 <arunprofessional123@gmail.com>
2022-01-12 11:57:32 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ export default class App extends React.Component {
       city: '',
       phone: ' ',
       age: ' ',
+      show: false,
     };
   }
   onChange = (e) => {
@@ -33,12 +34,9 @@ export default class App extends React.Component {
          city=${this.state.city}
          age=${this.state.age} `
       );
-      console.log(
-        `         name=${this.state.name}
-        phone =${this.state.phone}
-        city=${this.state.city}
-        age=${this.state.age} `
-      );
+      this.setState({
+        show: true,
+      });
     }
   };
 
@@ -47,25 +45,34 @@ export default class App extends React.Component {
     return (
       <>
         <form onSubmit={this.submitCheck}>
+          <label>Name</label>
           <input
             type="text"
             name="name"
             onChange={this.onChange}
             placeholder="enter the name"
           />
+          <br />
+          <label>Phone</label>
           <input
             type="text"
             name="phone"
             onChange={this.onChange}
             placeholder="enter the Phone Number"
           />
+          <br />
+          <label>city</label>
           <input
+            className="txt"
             type="text"
             name="city"
             placeholder="enter the city"
             onChange={this.onChange}
           />
+          <br />
+          <label>Age</label>
           <input
+            className="txt"
             type="text"
             name="age"
             placeholder="enter the age"
@@ -75,14 +82,19 @@ export default class App extends React.Component {
           <br />
           <button type="submit">submit</button>
         </form>
-        <div className="border">
-          <p>Name: {this.state.name}</p>
-          <p>Phone: {this.state.phone}</p>
-          <p>city: {this.state.city}</p>
-          <p>Age: {this.state.age}</p>
 
-          <div>{this.myfun}</div>
-        </div>
+        {this.state.show ? (
+          <div className="border">
+            <p>Name: {this.state.name}</p>
+            <p>Phone: {this.state.phone}</p>
+            <p>city: {this.state.city}</p>
+            <p>Age: {this.state.age}</p>
+
+            <div>{this.myfun}</div>
+          </div>
+        ) : (
+          ''
+        )}
       </>
     );
   }
